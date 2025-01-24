f4daf455d4197b58f8c0daf0fe3146e55dde6d49
HeXiaowei666 <1229235180@qq.com>
2025-01-24 17:11:00 +0800

    add autobuild.sh

---
+++ build/src/client/CMakeFiles/ChatClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/hexiaowei/cpp/chat_github/chatserver/bin/ChatClient"
+  "/home/hexiaowei/cpp/chat_github/chatserver/bin/ChatClient.pdb"
+  "CMakeFiles/ChatClient.dir/main.cpp.o"
+  "CMakeFiles/ChatClient.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
